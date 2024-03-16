ab82b29bd2dca131d72bb5a74d2b3a5d84adf992
zainabmirza-eng <zainabmirzafatah99@gmail.com>
2024-03-17 00:37:51 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+
+function findMatching(drivers, name){
+    const LowerCaseName = name.toLowerCase();
+    return drivers.filter((driver) => driver.toLowerCase()  === LowerCaseName); 
+}
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function matchName(drivers, name){
+    const LowerCaseName = name.toLowerCase();
+    return drivers.filter((driver) => driver.name.toLowerCase()  === LowerCaseName); 
+}
